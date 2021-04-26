b1e1e281f2fd6ff18fb6a9c52a2adc33c05d4098
priya4991 <pdutta@esri.com>
2021-04-26 21:35:35 +0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import '@esri/calcite-components/dist/calcite/calcite.css';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <calcite-button>button</calcite-button>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { applyPolyfills, defineCustomElements } from '@esri/calcite-components/dist/loader';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// Apply polyfills and then define the custom elements
+// polyfills are not needed if you don't support IE11 or Edge
+applyPolyfills().then(() => {
+  defineCustomElements(window);
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+  
+});
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
